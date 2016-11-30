9da573cfc5cb1d86e793445e155723aa4f82d819
drolmen <dm1396625260@163.com>
2016-11-30 11:53:06 +0800

    save,ready to join up thrid party

---
+++ SwiftZhiHuDayly/controllerLayer/SplashVC.swift
@@ -0,0 +1,35 @@
+//
+//  SplashVC.swift
+//  SwiftZhiHuDayly
+//
+//  Created by 55HaiTao on 2016/11/29.
+//
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
